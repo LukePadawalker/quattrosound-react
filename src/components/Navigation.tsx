ac3a3714166386a18e9f5bf8dd3ca9cde6cd198e
LukePadawalker <padurariu.luca@gmail.com>
2025-11-24 18:27:03 +0100

    fixed responsiveness problems

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,7 +18,7 @@ export default function Navigation({ onCartClick }: NavigationProps) {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 border-gray-800">
-      <div className="max-w-7xl mx-auto px-2 py-1 my-4 rounded-full sm:px-6 lg:px-24 bg-gradient-to-r from-cyan-700/90 to-black/90 ">
+      <div className=" mx-auto px-2 py-1 my-8 sm:mx-8 sm:px-8 md:px-16 md:mx-8 lg:px-16 xl:mx-24 2xl:mx-64 rounded-full bg-gradient-to-r from-blue-900/70 to-cyan-700/70  ">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
             <h1 className="text-3xl font-bold text-white">
@@ -26,7 +26,7 @@ export default function Navigation({ onCartClick }: NavigationProps) {
             </h1>
           </div>
 
-          <div className="hidden md:block">
+          <div className="hidden lg:block">
             <div className="ml-10 flex items-baseline space-x-8">
               <button onClick={() => scrollToSection('home')} className=" audiowide-regular text-white-400 hover:accent-gold transition-colors px-3 py-2 text-sm font-medium">
                 Home
@@ -61,7 +61,7 @@ export default function Navigation({ onCartClick }: NavigationProps) {
             </div>
           </div>
 
-          <div className="md:hidden flex items-center gap-4">
+          <div className="lg:hidden flex items-center gap-4">
             {/* <button
               onClick={onCartClick}
               className="relative text-gray-400 hover:accent-gold transition-colors"
@@ -85,7 +85,7 @@ export default function Navigation({ onCartClick }: NavigationProps) {
 
       {
         isOpen && (
-          <div className="md:hidden bg-gray-900 border-t border-gray-800">
+          <div className="lg:hidden  bg-gradient-to-r from-blue-900/90 to-cyan-700/90 border-t border-gray-800 px-8 mx-8 rounded-xl">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <button onClick={() => scrollToSection('home')} className="audiowide-regular text-white-400 hover:accent-gold block px-3 py-2 text-base font-medium w-full text-left">
                 Home
